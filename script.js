845f2b2b5328fdd29037ee858465fd1a5d27e1bf
AMRANE <amraneboukhabya3@gmail.com>
2025-12-09 13:19:47 +0000

    message pertinent de commit

--- script.js
+++ script.js
@@ -1,15 +1,13 @@
-<button id="theme-toggle">
+console.log("Le script est chargé !");
+// 1. CIBLER
+const titre = document.querySelector('#titre-principal');
 
-
-
-const themeButton = document.querySelector('#theme-toggle');
-
-
-if (themeButton) {
-  
-  themeButton.addEventListener('click', () => {
-   
-    document.body.classList.toggle('dark-mode');
+// On vérifie s'il existe (pour ne pas créer d'erreur sur les autres pages)
+if (titre) {
+  // 2. ÉCOUTER
+  titre.addEventListener('click', () => {
+    // 3. MANIPULER
+    console.log("J'ai cliqué sur le titre !");
+    titre.textContent = "Titre cliqué !";
   });
 }
-</button>
